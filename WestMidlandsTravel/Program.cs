70616f9a6d5afcf651ec54e05f90fb1312daed7f
OmarFarooqKhan <ofarooq767@gmail.com>
2022-10-17 02:33:01 +0100

    Init Part 2

--- WestMidlandsTravel/Program.cs
+++ WestMidlandsTravel/Program.cs
@@ -1,13 +1,18 @@
+using WestMidlandsTravel;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.RegisterConfigurations();
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.RegisterHttpClients();
+builder.Services.AddServices();
+builder.Services.AddResponseCaching();
+
 
 var app = builder.Build();
+
+app.UseResponseCaching();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
