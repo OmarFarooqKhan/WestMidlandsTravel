92a946c326a0f2840eb142b13978dfea2f713b25
OmarFarooqKhan <ofarooq767@gmail.com>
2025-05-18 23:33:00 +0100

    updates quickly to swagger

--- WestMidlandsTravel/Controllers/BusUpdatesController.cs
+++ WestMidlandsTravel/Controllers/BusUpdatesController.cs
@@ -13,17 +13,31 @@ public class BusUpdatesController : CustomController
         _logger = logger;
         _httpClient = httpClient;
     }
-    
+
     /// <summary>
     /// Gets a snapshot of the current bus network
     /// </summary>
     /// <returns></returns>
     [HttpGet("tripupdates/")]
     [ResponseCache(Duration = 120)]
-    public async Task<IActionResult> GetStopPredictions()
+    public async Task<IActionResult> GetStopPredictions(int start = 0, int end = 10)
     {
         var response = await _httpClient.GetAsync("trip_updates");
         _logger.LogInformation("Got response from GTFS Realtime API");
-        return Ok(response);
+        return Ok(response.Take(new Range(start, end)));
+    }
+
+    /// <summary>
+    /// Gets a tripId
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("tripId/")]
+    [ResponseCache(Duration = 120)]
+    public async Task<IActionResult> GetStopPredictionsById(int routeId)
+    {
+        var response = await _httpClient.GetAsync("trip_updates");
+        var outcome = response.First(entry => entry.TripUpdate.Trip.RouteId == routeId.ToString());
+        _logger.LogInformation("Got response from GTFS Realtime API");
+        return Ok(outcome);
     }
 }
